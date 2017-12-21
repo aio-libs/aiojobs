9d8d96404ef37e11444c4a815867af6f83fbdf78
1oomis <eugenebeseda@gmail.com>
2017-12-21 12:56:31 +0200

    #19: Add a new scheduler parameter for control pending jobs size

--- aiojobs/__init__.py
+++ aiojobs/__init__.py
@@ -5,13 +5,13 @@ from ._scheduler import Scheduler
 
 
 async def create_scheduler(*, close_timeout=0.1, limit=100,
-                           exception_handler=None):
+                           pending_limit=0, exception_handler=None):
     if exception_handler is not None and not callable(exception_handler):
         raise TypeError('A callable object or None is expected, '
                         'got {!r}'.format(exception_handler))
     loop = asyncio.get_event_loop()
     return Scheduler(loop=loop, close_timeout=close_timeout,
-                     limit=limit,
+                     limit=limit, pending_limit=pending_limit,
                      exception_handler=exception_handler)
 
 __all__ = ('create_scheduler',)
