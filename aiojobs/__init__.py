7dc383955cfa30db626100a6c0ff0280f33eff22
NODA, Kai <nodakai@gmail.com>
2018-05-17 20:25:31 +0800

    Add `loop` parameter to create_scheduler()

--- aiojobs/__init__.py
+++ aiojobs/__init__.py
@@ -13,11 +13,12 @@ from ._scheduler import Scheduler
 
 
 async def create_scheduler(*, close_timeout=0.1, limit=100,
-                           pending_limit=10000, exception_handler=None):
+                           pending_limit=10000, exception_handler=None, loop=None):
     if exception_handler is not None and not callable(exception_handler):
         raise TypeError('A callable object or None is expected, '
                         'got {!r}'.format(exception_handler))
-    loop = asyncio.get_event_loop()
+    if loop is None:
+        loop = asyncio.get_event_loop()
     return Scheduler(loop=loop, close_timeout=close_timeout,
                      limit=limit, pending_limit=pending_limit,
                      exception_handler=exception_handler)
