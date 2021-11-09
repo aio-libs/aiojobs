aed49745a416fb4fc657569a0f5ff5bf026996e1
Andrew Svetlov <andrew.svetlov@gmail.com>
2021-11-09 11:44:51 +0200

    Fix docs builder

--- docs/conf.py
+++ docs/conf.py
@@ -31,11 +31,11 @@ _version_path = os.path.abspath(
 with codecs.open(_version_path, "r", "latin1") as fp:
     try:
         _version_info = re.search(
-            r"^__version__ = '"
+            r'^__version__ = "'
             r"(?P<major>\d+)"
             r"\.(?P<minor>\d+)"
             r"\.(?P<patch>\d+)"
-            r"(?P<tag>.*)?'$",
+            r'(?P<tag>.*)?"$',
             fp.read(),
             re.M,
         ).groupdict()
