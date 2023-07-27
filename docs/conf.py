55d971c1d4cdbd7fa0d1fa1130c7e2b94f576e8d
Sam Bull <git@sambull.org>
2023-07-27 23:10:43 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -210,6 +210,6 @@ texinfo_documents = [
 # Example configuration for intersphinx:
 # refer to the Python standard library.
 intersphinx_mapping = {
-    "https://docs.python.org/3": None,
+    "python": ("https://docs.python.org/3": None),
     "aiohttp": ("https://aiohttp.readthedocs.io/en/stable/", None),
 }
