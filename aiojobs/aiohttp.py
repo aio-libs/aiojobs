58e16fa343da3ee7d3b576e40ae68500dfe27063
Juan Cruz <juancruzmencia@gmail.com>
2023-11-18 19:05:01 +0100

    Use AppKey in aiohttp 3.9 (#460)

--- aiojobs/aiohttp.py
+++ aiojobs/aiohttp.py
@@ -20,7 +20,8 @@ __all__ = ("setup", "spawn", "get_scheduler", "get_scheduler_from_app", "atomic"
 _T = TypeVar("_T")
 _RequestView = TypeVar("_RequestView", bound=Union[web.Request, web.View])
 
-# TODO(aiohttp 3.9+): Use AppKey
+
+AIOJOBS_SCHEDULER = web.AppKey("AIOJOBS_SCHEDULER", Scheduler)
 
 
 def get_scheduler(request: web.Request) -> Scheduler:
@@ -31,11 +32,11 @@ def get_scheduler(request: web.Request) -> Scheduler:
 
 
 def get_scheduler_from_app(app: web.Application) -> Optional[Scheduler]:
-    return app.get("AIOJOBS_SCHEDULER")
+    return app.get(AIOJOBS_SCHEDULER)
 
 
 def get_scheduler_from_request(request: web.Request) -> Optional[Scheduler]:
-    return request.config_dict.get("AIOJOBS_SCHEDULER")  # type: ignore[no-any-return]
+    return request.config_dict.get(AIOJOBS_SCHEDULER)
 
 
 async def spawn(request: web.Request, coro: Coroutine[object, object, _T]) -> Job[_T]:
@@ -62,7 +63,7 @@ def atomic(
 
 def setup(app: web.Application, **kwargs: Any) -> None:
     async def cleanup_context(app: web.Application) -> AsyncIterator[None]:
-        app["AIOJOBS_SCHEDULER"] = scheduler = Scheduler(**kwargs)
+        app[AIOJOBS_SCHEDULER] = scheduler = Scheduler(**kwargs)
         yield
         await scheduler.close()
 
